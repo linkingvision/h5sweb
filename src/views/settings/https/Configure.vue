d79739a3c11977a73e4a86b86038275ed4fee88e
ZHUZHENQUAN <zhuzhenquan@linkingvision.com>
2021-04-02 14:56:58 +0800

    完善代码

---
+++ src/views/settings/https/Configure.vue
@@ -0,0 +1,180 @@
+<template>
+    <div class="Cluster">
+        <div class="Cluster_block">
+            <div class="Cluster_block_title">HTTP配置</div>
+            <div class="Cluster_block_content">
+                <el-form class="Cluster_form" label-position="left" label-width="140px" :model="form">
+            
+                    <el-form-item label="HTTP端口">
+                        <el-input v-model="form.nHTTPPort"></el-input>
+                    </el-form-item>
+                    <el-form-item label="HTTPS端口">
+                        <el-input v-model="form.nHTTPSPort"></el-input>
+                    </el-form-item>
+                    <el-form-item label="SSL证书文件">
+                        <el-input v-model="form.strSSLCertificateFile"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                    </el-form-item>
+                </el-form>
+                <el-form class="Cluster_form" label-position="left" label-width="140px" :model="form">
+            
+                    <el-form-item :label="label.ForceHTTPS">
+                        <el-switch active-color="#13ce66" v-model="form.bForceHTTPS" @change='Together("bForceHTTPS",form.bForceHTTPS)'></el-switch>
+                    </el-form-item>
+                    <el-form-item :label="label.EnableDomain">
+                        <el-switch active-color="#13ce66" v-model="form.bEnableDomain" @change='Together("bEnableDomain",form.bEnableDomain)'></el-switch>
+                    </el-form-item>
+                    <el-form-item :label="label.DocumentRoot">
+                        <el-input v-model="form.strDocumentRoot" @change='Document(form.strDocumentRoot)'></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+        <!-- <div class="Cluster_block">
+            <div class="Cluster_block_title">{{$t("message.setting.ForceHTTPS")}}</div>
+            <div class="Cluster_block_content">
+                
+            </div>
+        </div> -->
+        <div class="Cluster_but">
+            <button @click="SetHTTPConf">{{$t("message.camera.save")}}</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Cluster",
+    data(){
+        return {
+            label:{
+                open_Close:this.$t("message.camera.open_Close"),
+                ForceHTTPS:this.$t("message.setting.ForceHTTPS"),
+                EnableDomain:this.$t("message.setting.EnableDomain"),
+                DocumentRoot:this.$t("message.setting.DocumentRoot"),
+            },
+            form: {
+                nHTTPPort:"8080",
+                nHTTPSPort:"8443",
+                strSSLCertificateFile:"server.pem",
+                bForceHTTPS:false,
+                bEnableDomain:false,
+                strDocumentRoot:"www/"
+            }
+        }
+    },
+    mounted(){
+        this.GetHTTPConf()
+
+    },
+    methods:{
+        //判断不能为空
+        Document(data){
+            console.log(data,this.form.strDocumentRoot)
+            if(data==''){
+                this.$message({
+                    message: '文件目录不能为空',
+                    type: 'warning'
+                });
+            }
+        },
+        Together(value,data){
+            console.log(value,data)
+            if(value=='bEnableDomain'){
+                if(data){
+                    this.form.bForceHTTPS=data
+                }
+            }else if(value=='bForceHTTPS'){
+                if(!data){
+                    this.form.bEnableDomain=data
+                }
+            }
+        },
+        SetHTTPConf(){
+            console.log(this.ruleForm,this.form)
+            // return
+            if(this.form.strDocumentRoot==''){
+                this.$message({
+                    message: '文件目录不能为空',
+                    type: 'warning'
+                });
+                return
+            }
+            var _this=this
+            var form=_this.form
+            var url = this.$store.state.IPPORT + "/api/v1/SetHTTPConf?http="+form.nHTTPPort+
+            "&https="+form.nHTTPSPort+
+            "&sslcertificatefile="+form.strSSLCertificateFile+
+            "&forcehttps="+form.bForceHTTPS+
+            "&enabledomain="+form.bEnableDomain+
+            "&documentroot="+form.strDocumentRoot+"&session="+ this.$store.state.token;
+
+            this.$http.get(url).then(result=>{
+                console.log("SetHTTPConf",result);
+                if(result.status == 200){
+                    if(result.data.bStatus){
+                        this.$root.bus.$emit('webrtc',true);
+                        this.$message({
+                            message: result.data.strCode,
+                            type: 'success'
+                        });
+                    }else{
+                        this.$message({
+                            message: result.data.strCode,
+                            type: 'warning'
+                        });
+                    }
+                }
+            })
+        },
+        GetHTTPConf(){
+            var _this=this
+            var url = this.$store.state.IPPORT + "/api/v1/GetHTTPConf?session="+ this.$store.state.token;
+            this.$http.get(url).then(result=>{
+                console.log("GetHTTPConf",result);
+                if(result.status == 200){
+                    this.form=result.data
+                }
+            })
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.Cluster{
+    padding: 40px;
+    .Cluster_block{
+        margin-bottom: 40px;
+        .Cluster_block_title{
+            margin-bottom: 20px;
+            font-size: 18px;
+            font-family: PingFang SC;
+            font-weight: 600;
+        }
+        .Cluster_block_content{
+            padding: 20px 40px;
+            .Cluster_form{
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+                .el-form-item{
+                    width: 30%;
+                }
+            }
+        }
+    }
+    .Cluster_but{
+        text-align: center;
+        button{
+            font-size: 16px;
+            padding: 8px 8%;
+            border: none;
+            background: #3ABBFE;
+            border-radius: 5px;
+        }
+    }
+}
+</style>
---
+++ src/views/settings/license/License.vue
@@ -0,0 +1,236 @@
+<template>
+	<div class="BatchImport">
+        <div class="BatchImport_flex">
+            <div class="BatchImport_title">
+                License{{lable.Import}}
+            </div>
+            <el-input placeholder="License文件" v-model="input">
+                <template slot="append">
+                    <!-- 按钮 -->
+                    <el-upload
+                    class="upload"
+                    action=""
+                    :multiple="false"
+                    :show-file-list="false"
+                    :http-request="httpRequest">
+                    <el-button size="small" class="iconfont icon-wenjianjia" type="primary"></el-button>
+                    </el-upload>
+                </template>
+            </el-input>
+            <!-- 按钮 end -->
+            <div class="BatchImport_butt">
+                <el-button size="small" @click="cancel" type="primary">{{lable.Cancel}}</el-button>
+                <el-button size="small" @click="UpLoadLic" type="primary">{{lable.Import}}</el-button>
+            </div>
+            <el-collapse v-model="activeNames">
+                <el-collapse-item title="详情" name="1">
+                    <div class="flex_nc_ag">
+                        <div class="flex_nc_cpu" v-for="(b,index) in capability" :key="index">
+                            <span class="cpu_zuo">{{b.name}}:</span>
+                            <span class="cpu_you" > {{b.id}}</span>
+                            
+                        </div>
+                    </div>
+                </el-collapse-item>
+            </el-collapse>
+        </div>
+     </div>
+</template>
+
+<script>
+import XLSX from 'xlsx'
+
+export default {
+	data () {
+		return {
+            activeNames:[],
+            tableData: [],
+            input:null,
+            file:null,
+            capability:null,
+            system:null,
+            lable:{
+                DeviceImport:this.$t("message.setting.DeviceImport"), 
+                Import:this.$t("message.setting.Import"),
+                Batchimportdevice:this.$t("message.setting.Batchimportdevice"),
+                Cancel: this.$t("message.setting.Cancel"),
+            },
+		}
+    },
+    mounted(){
+        this.GetSystemInfo()
+    },
+	methods: {
+        //版权
+        GetSystemInfo() {
+            let _this = this;
+            var url =_this.$store.state.IPPORT + "/api/v1/GetSystemInfo?session=" + _this.$store.state.token;
+            _this.$http.get(url).then(result => {
+                console.log(result);
+                if (result.status == 200) {
+                    var data =  result.data;
+                    var cpu=[{
+                        name:this.$t("message.dashboard.version"),
+                        id:data.strVersion
+                    },{
+                        name:this.$t("message.dashboard.hostid"),
+                        id:data.strHostId
+                    },{
+                        name:this.$t("message.dashboard.license"),
+                        id:data.strLicenseType
+                    },{
+                        name:this.$t("message.dashboard.channel"),
+                        id:data.strChannelLimit
+                    },{
+                        name:this.$t("message.dashboard.endtime"),
+                        id:data.strEndtime
+                    }]
+                    var fun=[{
+                        name:this.$t("message.dashboard.devicesdk"),
+                        id:data.bDeviceSDK
+                    },{
+                        name:this.$t("message.dashboard.transcoding"),
+                        id:data.bTranscoding
+                    },{
+                        name:this.$t("message.dashboard.gb28181"),
+                        id:data.bGB28181
+                    },{
+                        name:this.$t("message.dashboard.devicesdkpb"),
+                        id:data.bDeviceSDKPb
+                    },{
+                        name:this.$t("message.dashboard.cluster"),
+                        id:data.bCluster
+                    },{
+                        name:this.$t("message.dashboard.VideoConference"),
+                        id:data.bVideoConference
+                    },{
+                        name:this.$t("message.dashboard.Platform"),
+                        id:data.bPlatform
+                    },{
+                        name:this.$t("message.dashboard.Lingvideo"),
+                        id:data.bLingvideo
+                    },{
+                        name:this.$t("message.dashboard.Docker"),
+                        id:data.bDocker
+                    }]
+                    this.capability=cpu;
+                    this.system=fun;
+                    // console.log("***********",this.system)
+                }
+            }).catch(error => {
+                console.log("GetSystemInfo");
+            });
+        },
+        //导入
+	    httpRequest (e) {
+            let file = e.file // 文件信息
+            console.log('e: ', e)
+            console.log('file: ', e.file)
+            this.input=file.name
+            this.file=file
+        },
+        UpLoadLic(){
+            var url =this.$store.state.IPPORT + "/api/v1/UpLoadLic?session=" + this.$store.state.token;
+            this.$http.post(url,this.file,{headers: {'Content-Type': 'application/octet-stream'}}).then(result => {
+                console.log(result)
+                if (result.status == 200) {
+                    if(result.data.bStatus){
+                        this.GetSystemInfo()
+                        this.$root.bus.$emit('webrtc',true);
+                        this.$message({
+                            message: result.data.strCode,
+                            type: 'success'
+                        });
+                    }else{
+                        this.$message({
+                            message: result.data.strCode,
+                            type: 'warning'
+                        });
+                    }
+                }
+            }).catch(error => {
+                console.log("UpLoadLic",error);
+            });
+        },
+        //取消
+        cancel(){
+            this.input=null
+            this.file=null
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.BatchImport{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .BatchImport_flex{
+        width: 30%;
+        display: flex;
+        // justify-content: center;
+        flex-wrap: wrap;
+        // align-items: center;
+        margin-top: 10%;
+        .flex_nc_ag{
+            margin: 5px 0;
+            height:80%;
+            display: flex;
+            flex-wrap: wrap;
+            overflow-y: auto;
+            align-content: space-between;
+            .flex_nc_cpu{
+                width: 100%;
+                margin: 0 0 0 10px;
+                margin-bottom: 10px;
+                .cpu_zuo{
+                    font-size:12px;
+                    /* font-family:PingFang SC; */
+                    font-weight:600;
+                    margin-right: 8px;
+                    opacity:0.6;
+                }
+                .cpu_you{
+                    font-size:12px;
+                    /* font-family:PingFang SC; */
+                    font-weight:600;
+                }
+                #foo{
+                    border: 0;
+                    width: 78%;
+                    background: none;
+                }
+            }
+        }
+        .BatchImport_title{
+            font-size: 16px;
+            font-family: PingFang SC;
+            font-weight: 600;
+            margin-bottom: 60px;
+        }
+        .el-input{
+            width: 100%;
+            text-align: right;
+            margin-bottom: 30px;
+            .el-input-group__append{
+                    .el-button{
+                        font-size: 16px;
+                    }
+                }
+        }
+        .BatchImport_butt{
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+            button:nth-child(1){
+                background: none;
+                border: 1px solid #3ABBFE;
+            }
+            button:nth-child(2){
+                background: #3ABBFE;
+                border: none;
+            }
+        }
+    }
+}
+</style>
